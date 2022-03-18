919bcb7a5e9dfebdd4be49ceb748db611aa84496
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-03-18 15:08:07 -0300

    configurações de dep

--- src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/TrabalhoFinalModulo3Application.java
+++ src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/TrabalhoFinalModulo3Application.java
@@ -2,8 +2,10 @@ package br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TrabalhoFinalModulo3Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/exception/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3.exception;
+
+public class BusinessRuleException extends Exception {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
