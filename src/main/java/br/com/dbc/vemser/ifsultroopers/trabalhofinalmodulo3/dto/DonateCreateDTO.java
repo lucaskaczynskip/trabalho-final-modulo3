5d16f4ffc1f8bfac367450cc826c09ac1ed30612
Ana Vitória Göcthel <anagocthel@gmail.com>
2022-03-18 19:09:20 -0300

    Donate pronta

---
+++ src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/dto/DonateCreateDTO.java
@@ -0,0 +1,20 @@
+package br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3.dto;
+
+import br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3.entity.Request;
+import org.springframework.boot.context.properties.bind.Name;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class DonateCreateDTO {
+    @NotEmpty @NotNull
+    private String donator_name;
+    @Email
+    private String donator_email;
+    @NotNull @Min(1)
+    private Double donate_value;
+    @NotEmpty
+    private String description;
+}
