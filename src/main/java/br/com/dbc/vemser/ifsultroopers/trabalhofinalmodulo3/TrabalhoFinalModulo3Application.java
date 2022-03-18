96f45eb7868da3ecde227ca13678655bd826eea5
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-03-18 14:39:11 -0300

    estrutura inicial

---
+++ src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/TrabalhoFinalModulo3Application.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalModulo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalModulo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/TrabalhoFinalModulo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalModulo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
