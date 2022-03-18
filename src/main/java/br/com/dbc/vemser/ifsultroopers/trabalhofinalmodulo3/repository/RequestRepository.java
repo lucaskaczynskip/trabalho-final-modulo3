c20ca6f04bbdfa1fe6d507ffe60f07892333a087
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-03-18 16:16:19 -0300

    Request repository crud

--- src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/repository/RequestRepository.java
+++ src/main/java/br/com/dbc/vemser/ifsultroopers/trabalhofinalmodulo3/repository/RequestRepository.java
@@ -1,4 +1,47 @@
 package br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3.repository;
 
+import br.com.dbc.vemser.ifsultroopers.trabalhofinalmodulo3.entity.Request;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
 public class RequestRepository {
+
+    public static List<Request> list = new ArrayList<>();
+    public static AtomicInteger COUNTER = new AtomicInteger();
+
+    public List<Request> getAll() {
+        return list;
+    }
+
+    public Request getById(Integer id) throws Exception {
+        return list.stream()
+                .filter(request -> request.getIdRequest().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new Exception("Vakinha não encontrada."));
+    }
+
+    public Request create(Integer id, Request request) {
+        request.setIdUser(id);
+        list.add(request);
+        return request;
+    }
+
+    public Request update(Integer id, Request newData) throws Exception {
+        Request request = this.getById(id);
+        newData.setIdRequest(id);
+        newData.setIdUser(request.getIdUser());
+        list.add(list.indexOf(request), newData);
+        list.remove(request);
+        return newData;
+    }
+
+    public Request delete(Integer id) throws Exception {
+        Request request = this.getById(id);
+        list.remove(request);
+        return request;
+    }
 }
